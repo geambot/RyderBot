fdcc8b26b86fd51a11a68cc6c3c340d3357cc35d
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:14:56 +0700

    Update live.py

--- YukkiMusic/plugins/play/live.py
+++ YukkiMusic/plugins/play/live.py
@@ -10,10 +10,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from YukkiMusic import YouTube, app
-from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.stream.stream import stream
+from RyderBot import YouTube, app
+from RyderBot.utils.channelplay import get_channeplayCB
+from RyderBot.utils.decorators.language import languageCB
+from RyderBot.utils.stream.stream import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
