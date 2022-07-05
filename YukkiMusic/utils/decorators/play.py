85363b5b8b00a7573f63a0a2e2ad39edac5830ce
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 18:01:56 +0700

    Update play.py

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -11,15 +11,15 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
 from strings import get_string
-from YukkiMusic import YouTube, app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_cmode, get_lang,
+from RyderBot import YouTube, app
+from RyderBot.misc import SUDOERS
+from RyderBot.utils.database import (get_cmode, get_lang,
                                        get_playmode, get_playtype,
                                        is_active_chat,
                                        is_commanddelete_on,
                                        is_served_private_chat)
-from YukkiMusic.utils.database.memorydatabase import is_maintenance
-from YukkiMusic.utils.inline.playlist import botplaylist_markup
+from RyderBot.utils.database.memorydatabase import is_maintenance
+from RyderBot.utils.inline.playlist import botplaylist_markup
 
 
 def PlayWrapper(command):
