eeb5387d27a33e68bbcc54e8e654a2b55efadbe7
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:30:59 +0700

    Update mute.py

--- YukkiMusic/plugins/admins/mute.py
+++ YukkiMusic/plugins/admins/mute.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_muted, mute_on
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from RyderBot import app
+from RyderBot.core.call import RyderBot
+from RyderBot.utils.database import is_muted, mute_on
+from RyderBot.utils.decorators import AdminRightsCheck
 
 # Commands
 MUTE_COMMAND = get_command("MUTE_COMMAND")
@@ -34,7 +34,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"])
     await mute_on(chat_id)
-    await Yukki.mute_stream(chat_id)
+    await RyderBot.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention)
     )
