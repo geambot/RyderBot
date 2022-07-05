4ebbb3e33442f3aa1090fe5e869d103b84681a4b
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:12:11 +0700

    Update maintenance.py

--- YukkiMusic/plugins/sudo/maintenance.py
+++ YukkiMusic/plugins/sudo/maintenance.py
@@ -11,12 +11,12 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command, get_string
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_lang, is_maintenance,
+from RyderBot import app
+from RyderBot.misc import SUDOERS
+from RyderBot.utils.database import (get_lang, is_maintenance,
                                        maintenance_off,
                                        maintenance_on)
-from YukkiMusic.utils.decorators.language import language
+from RyderBot.utils.decorators.language import language
 
 # Commands
 MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
