2456cff3068deff6919b6ad841b3868e5ffe71ef
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:08:38 +0700

    Update block.py

--- YukkiMusic/plugins/sudo/block.py
+++ YukkiMusic/plugins/sudo/block.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_gban_user, remove_gban_user
-from YukkiMusic.utils.decorators.language import language
+from RyderBot import app
+from RyderBot.misc import SUDOERS
+from RyderBot.utils.database import add_gban_user, remove_gban_user
+from RyderBot.utils.decorators.language import language
 
 # Command
 BLOCK_COMMAND = get_command("BLOCK_COMMAND")
