0a36f27a3e63c857a04d053580e0debaffaaf28b
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:37:19 +0700

    Update Soundcloud.py

--- YukkiMusic/platforms/Soundcloud.py
+++ YukkiMusic/platforms/Soundcloud.py
@@ -12,7 +12,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from YukkiMusic.utils.formatters import seconds_to_min
+from RyderBot.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
