cb36f701dbf09799711975e08f731ebafa811ebe
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:14:17 +0700

    Update videomode.py

--- YukkiMusic/plugins/sudo/videomode.py
+++ YukkiMusic/plugins/sudo/videomode.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from RyderBot import app
+from RyderBot.misc import SUDOERS
+from RyderBot.utils.database import add_off, add_on
+from RyderBot.utils.decorators.language import language
 
 # Commands
 VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
