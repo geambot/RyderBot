7ba85cb2b8c52ab38d6087ec4ecfa48475466a65
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:13:02 +0700

    Update sudoers.py

--- YukkiMusic/plugins/sudo/sudoers.py
+++ YukkiMusic/plugins/sudo/sudoers.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_sudo, remove_sudo
-from YukkiMusic.utils.decorators.language import language
+from RyderBot import app
+from RyderBot.misc import SUDOERS
+from RyderBot.utils.database import add_sudo, remove_sudo
+from RyderBot.utils.decorators.language import language
 
 # Command
 ADDSUDO_COMMAND = get_command("ADDSUDO_COMMAND")
