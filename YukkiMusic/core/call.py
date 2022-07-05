bd5bdd9785eaba500cb310e90f9a7685ac132f64
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:39:45 +0700

    Update call.py

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -27,9 +27,9 @@ from pytgcalls.types.stream import StreamAudioEnded
 
 import config
 from strings import get_string
-from YukkiMusic import LOGGER, YouTube, app
-from YukkiMusic.misc import db
-from YukkiMusic.utils.database import (add_active_chat,
+from RyderBot import LOGGER, YouTube, app
+from RyderBot.misc import db
+from RyderBot.utils.database import (add_active_chat,
                                        add_active_video_chat,
                                        get_assistant,
                                        get_audio_bitrate, get_lang,
@@ -39,11 +39,11 @@ from YukkiMusic.utils.database import (add_active_chat,
                                        remove_active_chat,
                                        remove_active_video_chat,
                                        set_loop)
-from YukkiMusic.utils.exceptions import AssistantErr
-from YukkiMusic.utils.inline.play import (stream_markup,
+from RyderBot.utils.exceptions import AssistantErr
+from RyderBot.utils.inline.play import (stream_markup,
                                           telegram_markup)
-from YukkiMusic.utils.stream.autoclear import auto_clean
-from YukkiMusic.utils.thumbnails import gen_thumb
+from RyderBot.utils.stream.autoclear import auto_clean
+from RyderBot.utils.thumbnails import gen_thumb
 
 autoend = {}
 counter = {}
