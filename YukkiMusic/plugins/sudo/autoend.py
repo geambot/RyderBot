5ddfa341af3a8cf453584db4c4fa754f0b9c5b05
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:06:18 +0700

    Update autoend.py

--- YukkiMusic/plugins/sudo/autoend.py
+++ YukkiMusic/plugins/sudo/autoend.py
@@ -11,10 +11,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import autoend_off, autoend_on
-from YukkiMusic.utils.decorators.language import language
+from RyderBot import app
+from RyderBot.misc import SUDOERS
+from RyderBot.utils.database import autoend_off, autoend_on
+from RyderBot.utils.decorators.language import language
 
 # Commands
 AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
