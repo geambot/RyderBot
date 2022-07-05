9b4804cbc4b8b1c8fe665117492a575ae8e4a36d
geambot <87811835+geambot@users.noreply.github.com>
2022-07-05 19:13:35 +0700

    Update vars.py

--- YukkiMusic/plugins/sudo/vars.py
+++ YukkiMusic/plugins/sudo/vars.py
@@ -13,10 +13,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database.memorydatabase import get_video_limit
-from YukkiMusic.utils.formatters import convert_bytes
+from RyderBot import app
+from RyderBot.misc import SUDOERS
+from RyderBot.utils.database.memorydatabase import get_video_limit
+from RyderBot.utils.formatters import convert_bytes
 
 VARS_COMMAND = get_command("VARS_COMMAND")
 
